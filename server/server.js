16b839c2c8fe98c4d975d4689578d1bc8bd11272
soumir21 <soumirjyoti@gmail.com>
2024-06-01 17:09:14 +0530

    copied from the chat

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use("/api/product",productRouter);
 app.use("/api/payment",paymentRouter);
 app.use("/api/order",orderRouter);
 app.use("/api/items",itemRouter)
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.use(errorMiddleware)
 
@@ -28,5 +28,5 @@ app.get("*",(req,res)=>{
 
 
 dbConenct().then(app.listen(port,()=>{
-    console.log("started listeniong at 5000")
+    console.log("started listeniong at ",port)
 }))
